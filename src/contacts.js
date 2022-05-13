6bb1ddd28abb7bc4b27876de93d2cdf07f0a457f
Daniil Genneke <genneke@mail.ru>
2022-05-13 23:52:58 +0300

    opt

--- src/contacts.js
+++ src/contacts.js
@@ -1,19 +1,22 @@
 const contacts = document.getElementsByClassName("contacts")[0];
 const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
 
+let items = [];
+
 function addContacts() {
   const fragment = document.createDocumentFragment();
-  for (let i = 0; i < 50000; i++) {
+  for (let i = 0; i < 50; i++) {
     const child = document.createElement("div");
     child.textContent = i;
     child.classList.add("contact");
     fragment.appendChild(child);
+    items.push(child);
   }
   contacts.appendChild(fragment);
 }
 
 contacts.addEventListener("scroll", (e) => {
-  const items = Array.from(contacts.getElementsByClassName("contact"));
+  //const items = Array.from(contacts.getElementsByClassName("contact"));
   const itemOffsets = items.map((item) => item.offsetTop);
   const topItemIndex = itemOffsets.findIndex(
     (offset) => contacts.scrollTop - offset <= -18
